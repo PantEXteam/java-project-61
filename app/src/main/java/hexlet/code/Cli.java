3543058e7aa0ba70bfca80d62774aa7692401085
Lappo Panteleymon <p.lappo@marvel.ru>
2025-05-11 19:39:02 +0300

    main: Добавление файла для SonarQube

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public final class Cli {
+    public static void greetings() {
+        System.out.println("Welcome to the Brain Games!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Hello, " + name + "!");
+    }
+}
